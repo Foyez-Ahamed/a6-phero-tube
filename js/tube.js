a3834cb6023794ab91eba896c5917c6ba8f4a661
Foyez-Ahamed <foyez1435@gmail.com>
2023-09-02 18:44:44 +0600

    update project

--- js/tube.js
+++ js/tube.js
@@ -59,7 +59,7 @@ const displayCategoryVideo = (categoryVideo) => {
   
           showVideosDiv.innerHTML = `
   
-          <figure> <img class = "lg:w-full lg:h-[170px]" src="${showVideos?.thumbnail}"/> <span class = "absolute bg-neutral-900 text-slate-200 rounded-lg top-[40%] ${(showVideos.others.posted_date <= 0)? "hidden" : " "} right-2 p-1"> ${(finalHours > 0 && finalMinute > 0)? finalHours + " hrs " + finalMinute + " Minute " + "ago" : ""} </span> </figure>
+          <figure> <img class = "w-full lg:w-full md:w-full  md:h-[170px] lg:h-[170px]" src="${showVideos?.thumbnail}"/> <span class = "absolute bg-neutral-900 text-slate-200 rounded-lg top-[45%] md:top-[40%] lg:top-[40%] ${(showVideos.others.posted_date <= 0)? "hidden" : " "} right-2 p-1"> ${(finalHours > 0 && finalMinute > 0)? finalHours + " hrs " + finalMinute + " Minute " + "ago" : ""} </span> </figure>
               <div class="p-6">
                 <h2 class ="flex justify-start items-center gap-2">
                  <img class = "w-[40px] h-[40px] rounded-full" src = "${showVideos?.authors[0].profile_picture}" />
@@ -96,7 +96,7 @@ const sortByViews = async () => {
   
           showVideosDiv.innerHTML = `
   
-          <figure> <img class = "lg:w-full lg:h-[170px]" src="${showVideos?.thumbnail}"/> <span class = "absolute bg-neutral-900 text-slate-200 rounded-lg top-[40%] ${(showVideos.others.posted_date <= 0)? "hidden" : " "} right-5 p-1"> ${(finalHours > 0 && finalMinute > 0)? finalHours + " hrs " + finalMinute + " Minute " + "ago" : ""}  </span> </figure>
+          <figure> <img class = "w-full lg:w-full md:w-full  md:h-[170px] lg:h-[170px]" src="${showVideos?.thumbnail}"/> <span class = "absolute bg-neutral-900 text-slate-200 rounded-lg top-[45%] md:top-[40%] lg:top-[40%] ${(showVideos.others.posted_date <= 0)? "hidden" : " "} right-2 p-1"> ${(finalHours > 0 && finalMinute > 0)? finalHours + " hrs " + finalMinute + " Minute " + "ago" : ""} </span> </figure>
               <div class="p-6">
                 <h2 class ="flex justify-start items-center gap-2">
                  <img class = "w-[40px] h-[40px] rounded-full" src = "${showVideos?.authors[0].profile_picture}" />
