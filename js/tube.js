4b5e6a060bbbde03362e0e5261f00c6b8da75e37
Foyez-Ahamed <foyez1435@gmail.com>
2023-09-02 15:28:23 +0600

    drawing button err message show added

--- js/tube.js
+++ js/tube.js
@@ -7,6 +7,7 @@ const handleVideosCategories = async () => {
 
 
 const displayVideosCategories = (videosCategories) => {
+   
     const categoriesContainer = document.getElementById('categories-container');
 
     videosCategories.forEach(categories => {
@@ -25,6 +26,7 @@ const displayVideosCategories = (videosCategories) => {
 
 
 const handleVideosCategory = async (categoryId) => {
+    // console.log(categoryId);
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
     const categoryVideo = data.data;
@@ -33,35 +35,43 @@ const handleVideosCategory = async (categoryId) => {
 
 
 const displayCategoryVideo = (categoryVideo) => {
-    console.log(categoryVideo);
-    const showVideoContainer =  document.getElementById('show-video-container');
 
-    showVideoContainer.textContent = "";
+       const drawingId = document.getElementById('drawing-err-container');
+       if(categoryVideo.length === 0){
+          drawingId.classList.remove('hidden')
+       } else{
+          drawingId.classList.add('hidden')
+       }
+   
+        const showVideoContainer =  document.getElementById('show-video-container');
 
-    categoryVideo.forEach(showVideos => {
-        // console.log(showVideos);
+        showVideoContainer.textContent = "";
 
-        const showVideosDiv = document.createElement("div");
-        showVideosDiv.classList = `card`;
-
-        showVideosDiv.innerHTML = `
-
-        <figure> <img class = "lg:w-full lg:h-[170px]" src="${showVideos?.thumbnail}"/> <span class = "absolute bg-black text-white rounded-md top-[40%] right-5">${showVideos?.others.posted_date}</span> </figure>
-            <div class="p-6">
-              <h2 class ="flex justify-start items-center gap-2">
-               <img class = "w-[40px] h-[40px] rounded-full" src = "${showVideos?.authors[0].profile_picture}" />
-                <div class = "text-md font-bold">${showVideos.title}</div>
-              </h2>
-              <div class="flex justify-start items-center mt-3 ml-9">
-                <div class="badge ">${showVideos?.authors[0]?.profile_name}</div> 
-                <div class="badge "> ${showVideos?.authors[0]?.verified ? `<img src="./icons/verified.svg" alt=""></img>` : ''} </div>
+        categoryVideo.forEach(showVideos => {
+          const showVideosDiv = document.createElement("div");
+          showVideosDiv.classList = `card`;
+  
+          showVideosDiv.innerHTML = `
+  
+          <figure> <img class = "lg:w-full lg:h-[170px]" src="${showVideos?.thumbnail}"/> <span class = "absolute bg-black text-white rounded-md top-[40%] right-5"></span> </figure>
+              <div class="p-6">
+                <h2 class ="flex justify-start items-center gap-2">
+                 <img class = "w-[40px] h-[40px] rounded-full" src = "${showVideos?.authors[0].profile_picture}" />
+                  <div class = "text-md font-bold">${showVideos.title}</div>
+                </h2>
+                <div class="flex justify-start items-center mt-3 ml-9">
+                  <div class="badge ">${showVideos?.authors[0]?.profile_name}</div> 
+                  <div class="badge "> ${showVideos?.authors[0]?.verified ? `<img src="./icons/verified.svg" alt=""></img>` : ''} </div>
+                </div>
+                <div class = "mt-3 ml-11">${showVideos.others.views} views</div>
               </div>
-              <div class = "mt-3 ml-11">${showVideos.others.views} views</div>
-            </div>
+          
+          `
+          showVideoContainer.appendChild(showVideosDiv);
+      });
+
         
-        `
-        showVideoContainer.appendChild(showVideosDiv);
-    });
+    
 };
 
 
